792cf098a6de2b278437ca34f189e57276a1f87d
ajin12 <amy_jin@college.harvard.edu>
2019-07-08 17:56:24 -0700

    take photo using camera and upload to parse

--- app/src/main/java/com/example/instagram/HomeActivity.java
+++ app/src/main/java/com/example/instagram/HomeActivity.java
@@ -1,11 +1,21 @@
 package com.example.instagram;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.annotation.Nullable;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.example.instagram.model.Post;
 import com.parse.FindCallback;
@@ -19,10 +29,17 @@ import java.util.List;
 
 public class HomeActivity extends AppCompatActivity {
 
-    private static final String imagePath = "~/Users/ayjin/Desktop/screenshot.png";
+//    private static final String imagePath = "~/Users/ayjin/Desktop/screenshot.png";
     private EditText descriptionInput;
     private Button createButton;
     private Button refreshButton;
+    private Button btnCaptureImage;
+    private ImageView ivPreview;
+
+    public final String APP_TAG = "MyCustomApp";
+    public final static int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 1034;
+    public String photoFileName = "photo.jpg";
+    File photoFile;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,6 +50,15 @@ public class HomeActivity extends AppCompatActivity {
         descriptionInput = findViewById(R.id.etDescription);
         createButton = findViewById(R.id.btnCreate);
         refreshButton = findViewById(R.id.btnRefresh);
+        btnCaptureImage = findViewById(R.id.btnCaptureImage);
+        ivPreview = findViewById(R.id.ivPreview);
+
+        btnCaptureImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                launchCamera();
+            }
+        });
 
         createButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -42,9 +68,13 @@ public class HomeActivity extends AppCompatActivity {
 
                 // TODO - ask user to select file or take a photo using camera
                 // create a file for the image
-                final File file = new File(imagePath);
-                final ParseFile parseFile = new ParseFile(file);
+                final ParseFile parseFile = new ParseFile(photoFile);
 
+                if (photoFile == null || ivPreview.getDrawable() == null) {
+                    Log.e(APP_TAG, "No photo to submit");
+                    Toast.makeText(v.getContext(), "There is no photo", Toast.LENGTH_SHORT).show();
+                    return;
+                }
                 createPost(description, parseFile, user);
             }
         });
@@ -59,6 +89,59 @@ public class HomeActivity extends AppCompatActivity {
         loadTopPosts();
     }
 
+    private void launchCamera() {
+        // create Intent to take a picture and return control to the calling application
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Create a File reference to access to future access
+        photoFile = getPhotoFileUri(photoFileName);
+
+        // wrap File object into a content provider
+        // required for API >= 24
+        // See https://guides.codepath.com/android/Sharing-Content-with-Intents#sharing-files-with-api-24-or-higher
+        Uri fileProvider = FileProvider.getUriForFile(this, "com.codepath.fileprovider", photoFile);
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, fileProvider);
+
+        // If you call startActivityForResult() using an intent that no app can handle, your app will crash.
+        // So as long as the result is not null, it's safe to use the intent.
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            // Start the image capture intent to take photo
+            startActivityForResult(intent, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if (requestCode == CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE) {
+            if (resultCode == RESULT_OK) {
+                // by this point we have the camera photo on disk
+                Bitmap takenImage = BitmapFactory.decodeFile(photoFile.getAbsolutePath());
+                // RESIZE BITMAP, see section below
+                // Load the taken image into a preview
+                ivPreview.setImageBitmap(takenImage);
+            } else { // Result was a failure
+                Toast.makeText(this, "Picture wasn't taken!", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    // Returns the File for a photo stored on disk given the fileName
+    public File getPhotoFileUri(String fileName) {
+        // Get safe storage directory for photos
+        // Use `getExternalFilesDir` on Context to access package-specific directories.
+        // This way, we don't need to request external read/write runtime permissions.
+        File mediaStorageDir = new File(getExternalFilesDir(Environment.DIRECTORY_PICTURES), APP_TAG);
+
+        // Create the storage directory if it does not exist
+        if (!mediaStorageDir.exists() && !mediaStorageDir.mkdirs()){
+            Log.d(APP_TAG, "failed to create directory");
+        }
+
+        // Return the file target for the photo based on filename
+        File file = new File(mediaStorageDir.getPath() + File.separator + fileName);
+
+        return file;
+    }
+
     private void createPost(String description, ParseFile imageFile, ParseUser user) {
         final Post newPost = new Post();
         newPost.setDescription(description);
@@ -71,6 +154,8 @@ public class HomeActivity extends AppCompatActivity {
             public void done(ParseException e) {
                 if (e == null) {
                     Log.d("HomeActivity", "Create post success!");
+                    descriptionInput.setText("");
+                    ivPreview.setImageResource(0);
                 } else {
                     e.printStackTrace();
                 }
